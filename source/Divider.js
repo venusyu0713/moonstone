6666baad17b262089e94a762546e0aceb6013c0e
Krishna Rangarajan <krishna.rangarajan@lge.com>
2014-07-10 17:45:01 -0700

    batch 2-1 initial pass

--- source/Divider.js
+++ source/Divider.js
@@ -1,12 +1,39 @@
-/**
-	_moon.Divider_ is a simple styled component that may be used as a separator
-	between groups of components.
-*/
-enyo.kind({
-	name: "moon.Divider",
-	//* @protected
-	classes: "moon-divider moon-divider-text",
-	mixins: ["moon.MarqueeSupport", "moon.MarqueeItem"],
-	marqueeOnSpotlight: false,
-	marqueeOnRender: true
-});
+(function (enyo, scope) {
+	/**
+		_moon.Divider_ is a simple styled component that may be used as a separator
+		between groups of components.
+
+	* @class moon.Divider
+	* @mixes moon.MarqueeSupport
+	* @public
+	*/
+	enyo.kind(
+		/** @lends moon.Divider.prototype */ {
+
+		/**
+		* @private
+		*/
+		name: 'moon.Divider',
+
+		/**
+		* @private
+		*/
+		classes: 'moon-divider moon-divider-text',
+
+		/**
+		* @private
+		*/
+		mixins: ['moon.MarqueeSupport', 'moon.MarqueeItem'],
+
+		/**
+		* @private
+		*/
+		marqueeOnSpotlight: false,
+
+		/**
+		* @private
+		*/
+		marqueeOnRender: true
+	});
+
+})(enyo, this);
