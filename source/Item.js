cfe0beb2c3c16e78746fa596efdb6c358ae6a944
Krishna Rangarajan <krishna.rangarajan@lge.com>
2014-07-14 18:20:32 -0700

    Icon to Item (prelim

--- source/Item.js
+++ source/Item.js
@@ -1,38 +1,41 @@
-/**
-	_moon.Item_ is a focusable Moonstone-styled control that can display simple
-	text or a set of controls.
-*/
-enyo.kind({
-	name: "moon.Item",
-	//* @protected
-	classes: "moon-item",
-	mixins: ["moon.MarqueeSupport", "moon.MarqueeItem"],
-	spotlight: true,
-	handlers: {
-		onSpotlightFocused: "spotlightFocused"
-	},
-	//* @public
-	published: {
-		/**
-			When true, the control is shown as disabled and does not generate tap
-			events
-		*/
-		disabled: false
-	},
-	//* @protected
-	create: function() {
-		this.inherited(arguments);
-		this.disabledChanged();
-		if (this.children.length) {
-			this.addClass("allow-wrap");
+(function (enyo, scope) {
+	/**
+		_moon.Item_ is a focusable Moonstone-styled control that can display simple
+		text or a set of controls.
+	*/
+	enyo.kind({
+		name: 'moon.Item',
+		//* @protected
+		classes: 'moon-item',
+		mixins: ['moon.MarqueeSupport', 'moon.MarqueeItem'],
+		spotlight: true,
+		handlers: {
+			onSpotlightFocused: 'spotlightFocused'
+		},
+		//* @public
+		published: {
+			/**
+				When true, the control is shown as disabled and does not generate tap
+				events
+			*/
+			disabled: false
+		},
+		//* @protected
+		create: function () {
+			this.inherited(arguments);
+			this.disabledChanged();
+			if (this.children.length) {
+				this.addClass('allow-wrap');
+			}
+		},
+		disabledChanged: function (inOld) {
+			this.addRemoveClass('disabled', this.disabled);
+		},
+		spotlightFocused: function (inSender, inEvent) {
+			if (inEvent.originator === this) {
+				this.bubble('onRequestScrollIntoView');
+			}
 		}
-	},
-	disabledChanged: function(inOld) {
-		this.addRemoveClass("disabled", this.disabled);
-	},
-	spotlightFocused: function(inSender, inEvent) {
-		if (inEvent.originator === this) {
-			this.bubble("onRequestScrollIntoView");
-		}
-	}
-});
+	});
+
+})(enyo, this);
